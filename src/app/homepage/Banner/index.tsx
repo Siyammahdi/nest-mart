0698784e91fccce442664ba23255a5ca969810f0
Siyammahdi <siyammahdi1@gmail.com>
2026-01-01 23:35:02 +0600

    categories responsive updated

--- src/app/homepage/Banner/index.tsx
+++ src/app/homepage/Banner/index.tsx
@@ -5,15 +5,17 @@ import Ad from './Ad';
 
 const Banner = () => {
     return (
-        <div className='my-8 mx-4 flex flex-col lg:flex-row items-center gap-6'>
-            <div className='w-full md:w-[15%]'>
+        <div className='w-[100%] my-8 mx-4 flex flex-col lg:flex-row items-center gap-6'>
+            <div className='w-[30%]'>
                 <Categories />
             </div>
-            <div className='w-full md:w-[60%]'>
-                <Carousel />
-            </div>
-            <div className='w-full md:w-[25%]'>
-                <Ad />
+            <div className='w-full flex flex-col md:flex-row items-center gap-6'>
+                <div className='w-full md:w-[60%]'>
+                    <Carousel />
+                </div>
+                <div className='w-full md:w-[25%]'>
+                    <Ad />
+                </div>
             </div>
         </div>
     );
